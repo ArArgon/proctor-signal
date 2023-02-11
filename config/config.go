9196160f32ddfe6c25b0427c26e1f92f39b45be5
ArArgon <liaozping@gmail.com>
2023-02-11 18:10:57 +0800

    fix: mandate `ResourceFactor`

--- config/config.go
+++ config/config.go
@@ -25,7 +25,7 @@ type LanguageConfEntity struct {
 	CompileSpaceLimit uint64            `yaml:"CompileSpaceLimit"`
 	ExecuteCmd        string            `yaml:"ExecuteCmd"`
 	Environment       []string          `yaml:"Environment"`
-	ResourceFactor    uint64            `default:"1" yaml:"ResourceFactor"`
+	ResourceFactor    uint64            `yaml:"ResourceFactor"`
 	Options           map[string]string `yaml:"Options"`
 }
 
